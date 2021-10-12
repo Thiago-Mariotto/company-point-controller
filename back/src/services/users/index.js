3583b2c6cc4873ff781ae016bc0b92e5d9492602
thiago-mariotto <thiagomariotto@outlook.com.br>
2021-10-12 09:51:26 -0300

    add user service

--- back/src/services/users/index.js
+++ back/src/services/users/index.js
@@ -1,7 +1,33 @@
-const register = async () => {
+const userModel = require('../../models/users');
 
-}
+const list = async () => {
+  const users = await userModel.users();
+
+  return users;
+};
+
+const register = async (user) => {
+  const { name, lastname, role, jobId, salary } = user;
+  const createdId = await userModel.register(name, lastname, role, jobId, salary);
+  
+  const newUser = {
+    id: createdId,
+    name,
+  };
+  
+  return newUser;
+};
+
+const update = async (user) => {
+  const { name, email, password, role } = user;
+
+  const updatedUser = await userModel.update(name, email, password, role);
+
+  return updatedUser;
+};
 
 module.exports = {
-  register
-}
+  list,
+  register,
+  update,
+};
