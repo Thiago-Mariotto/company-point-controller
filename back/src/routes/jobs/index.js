a62248d956ec93de0b220ec0d5fc99909a86e17f
thiago-mariotto <thiagomariotto@outlook.com.br>
2021-10-12 12:57:14 -0300

    add other routes to jobs

--- back/src/routes/jobs/index.js
+++ back/src/routes/jobs/index.js
@@ -4,7 +4,9 @@ const jobs = require('../../controllers/jobs');
 const jobRouter = Router();
 
 jobRouter.get('/', jobs.list);
+jobRouter.get('/:id', jobs.find);
 jobRouter.post('/', jobs.register);
 jobRouter.put('/:id', jobs.update);
+jobRouter.delete('/:id', jobs.remove);
 
 module.exports = jobRouter;
