d67a94dacca982fb712cff4e2ad62de0ceea6eda
thiago-mariotto <thiagomariotto@outlook.com.br>
2021-10-12 12:57:33 -0300

    change routes on users

--- back/src/routes/users/index.js
+++ back/src/routes/users/index.js
@@ -1,9 +1,16 @@
 const { Router } = require('express');
 const users = require('../../controllers/users');
+const {
+  isAdmin,
+  validateToken,
+} = require('../../middlewares');
 
 const userRouter = Router();
 
-userRouter.get('/', users.list);
+userRouter.get('/', validateToken, isAdmin, users.list);
+userRouter.get('/:id', users.find);
 userRouter.post('/', users.register);
+userRouter.put('/', users.update);
+userRouter.delete('/:id', users.remove);
 
 module.exports = userRouter;
